9bae40df786bf7f2421ba69530ca868d72c6ec24
likuozhan <lijia9512@163.com>
2021-07-20 16:12:17 +0800

    力扣1824 lkz

--- data-algorithm/src/main/java/com/demo/LeeCodeStudy2.java
+++ data-algorithm/src/main/java/com/demo/LeeCodeStudy2.java
@@ -2,12 +2,34 @@ package com.demo;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class LeeCodeStudy2 {
+
+
+    /**
+     * 数组中最大数对和的最小值
+     * 一个数对 (a,b) 的 数对和 等于 a + b 。最大数对和 是一个数对数组中最大的 数对和 。
+     *
+     * 比方说，如果我们有数对 (1,5) ，(2,3) 和 (4,4)，最大数对和 为 max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8 。
+     * 给你一个长度为 偶数 n 的数组 nums ，请你将 nums 中的元素分成 n / 2 个数对，使得：
+     * nums 中每个元素 恰好 在 一个 数对中，且
+     * 最大数对和 的值 最小 。
+     * 请你在最优数对划分的方案下，返回最小的 最大数对和 。
+     */
+    @Test
+    public void maxSumArr(){
+        int[] nums={1,5,2,3};
+        Arrays.sort(nums);
+        int i = 0;
+        int j = nums.length - 1;
+        int res = 0;
+        while (i < j) {
+            res = Math.max(nums[i++] + nums[j--], res);
+        }
+        System.out.println(res);
+    }
+
 
 
     /**
