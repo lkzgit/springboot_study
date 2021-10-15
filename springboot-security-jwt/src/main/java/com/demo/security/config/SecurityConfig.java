8cfbebb5c957842cf55717e37232898a8ea82189
likuozhan <likuozhan>
2021-10-15 17:34:35 +0800

    springboot_redis模块继承springcache

--- springboot-security-jwt/src/main/java/com/demo/security/config/SecurityConfig.java
+++ springboot-security-jwt/src/main/java/com/demo/security/config/SecurityConfig.java
@@ -31,6 +31,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  * 开启权限注解,默认是关闭的 当@EnableGlobalMethodSecurity(prePostEnabled=true)的时候，@PreAuthorize可以使用
  * 这里主要@PreAuthorize, @PostAuthorize, @Secured这三个注解可以使用
  * 当@EnableGlobalMethodSecurity(securedEnabled=true)的时候，@Secured可以使用：
+ *
+ * @EnableOAuth2Sso 开启单点登录
  */
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
