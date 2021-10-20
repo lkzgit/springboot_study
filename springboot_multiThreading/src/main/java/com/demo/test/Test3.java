687d8e93d020e61e867049fab8cea72094fec95a
likuozhan <likuozhan>
2021-10-20 17:32:47 +0800

    交替输出奇偶数 kk

--- springboot_multiThreading/src/main/java/com/demo/test/Test3.java
+++ springboot_multiThreading/src/main/java/com/demo/test/Test3.java
@@ -2,6 +2,8 @@ package com.demo.test;
 
 import com.sun.org.apache.bcel.internal.generic.NEW;
 
+import java.util.concurrent.locks.LockSupport;
+
 /**
  * @author lkz
  * @date 2021/10/20 16:49
@@ -9,13 +11,34 @@ import com.sun.org.apache.bcel.internal.generic.NEW;
  */
 public class Test3 {
 
+    private static Thread t1=null,t2=null;
     public static void main(String[] args) {
-        final Test3 test3=new Test3();
-        Thread t1 = new Thread(test3::print1);
-        Thread t2 = new Thread(test3::print2);
 
-        t1.start();
-        t2.start();
+
+        char [] a="123456".toCharArray();
+        char [] b="zxcvbn".toCharArray();
+        t1= new Thread(()->{
+            for(int i=0;i<a.length;i++){
+                System.out.println(a[i]);
+                LockSupport.unpark(t2);
+                LockSupport.park();
+            }
+        },"t1");
+         t2=new Thread(()->{
+            for(int i=0;i<b.length;i++){
+                System.out.println(b[i]);
+                LockSupport.unpark(t1);
+                LockSupport.park();
+            }
+        },"t2");
+         t1.start();
+         t2.start();
+//        final Test3 test3=new Test3();
+//        Thread t1 = new Thread(test3::print1);
+//        Thread t2 = new Thread(test3::print2);
+//
+//        t1.start();
+//        t2.start();
 
     }
 
